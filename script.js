67363ceeed9964876a1e0da3c4a1754aa93e7816
xza <584788096@qq.com>
2018-01-09 18:38:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+var n=0 ,m=0;
+
 $('.click-me').on('click',function(e){
-  e.stopPropagation();
-  $('.popover-in').show();
-  setTimeout(function(){
-    $('.popover').on('click',function(e){
+   $('.popover-in').show();
+      $('.popover-in').on('click',function(e){
       e.stopPropagation();
-    })
+    });
+  if(m%2===n%2){
+  setTimeout(function(){
     $(document).one('click',function(){
       $('.popover-in').hide();
+      m=n;
     });
   },0);
+  }
+      n++;
+});
+ $(document).one('click',function(){
+     m++;
 });
